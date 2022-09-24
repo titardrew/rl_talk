6d591604bd60b11c2f9c488ca1430708fde7124a
Andrii Tytarenko <andrii.tytarenko@ringteam.com>
2022-09-24 05:48:32 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="talk",
+    version="0.0.1",
+    description="",
+    author="Andrii Tytarenko",
+    author_email="titarenkoan@gmail.com",
+    py_modules=["lib"],
+)
